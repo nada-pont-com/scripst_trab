24cb47baa3cb961338bc40a90ce6f8f62fe5ed09
nada-pont-com <42653635+nada-pont-com@users.noreply.github.com>
2022-02-08 23:16:58 -0300

    Update ab.sh

--- ab.sh
+++ ab.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-$(ab -n 10000 -c 100 https://unionleitor.top/) >> "teste1.txt"
-$(ab -n 10000 -c 100 https://www.google.com/) >> "teste2.txt"
+$(ab -n 10000 -c 100 https://unionleitor.top/) >> "saida1.txt"
+$(ab -n 10000 -c 100 https://www.google.com/) >> "saida2.txt"
